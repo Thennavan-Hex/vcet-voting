6142e0cdad2fc305136348e17bbaec06afd54229
Abis2022 <118505951+Abis2022@users.noreply.github.com>
2022-11-21 14:31:59 +0530

    main.c updated

--- MAIN.C
+++ MAIN.C
@@ -1,6 +1,13 @@
 #include"election.h"
+#include <windows.h>
 
 int main(){
+    printf("\n\n\n");
+    printf("\t\t\t  *******  VELALAR COLLEGE OF ENGINEERING AND TECHNOLOGY  *******\n\n");
+    printf("\t\t\t      ******    DEPARTMENT ELECTION BASIC PORTAL      ****** \n\n");
+    printf("\t\t\t             **************************************** \n");
+    Sleep(1000);
+
     while(1){
         printf("\n\t\t\t   1.Student panel \n\t\t\t   2.Admin panel \n\t\t\t   3.Exit \n\t\t\t   Option:");
 		char input;
